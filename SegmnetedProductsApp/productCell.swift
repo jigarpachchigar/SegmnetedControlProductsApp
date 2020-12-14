dae256ebb90ec7209fabd2997c92b445f497a400
Jigar <Jigarpachchigar5561@gmail.com>
2020-12-14 16:47:32 +0530

    continue

---
+++ SegmnetedProductsApp/productCell.swift
@@ -0,0 +1,21 @@
+//
+//  productCell.swift
+//  SegmnetedProductsApp
+//
+//  Created by Jigar Pachchigar on 13/12/20.
+//
+
+import UIKit
+
+class productCell: UITableViewCell {
+   
+    
+    
+    @IBAction func showcart(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+}
